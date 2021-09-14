f5517d5b9f7c8dd84583dd00e65f2a08b8cb1f27
nape <najinshah.pearson@gmail.com>
2021-09-14 22:44:23 +0530

    new files added

--- index.js
+++ index.js
@@ -1,34 +1,30 @@
-// Import stylesheets
-import './style.css';
-
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `${navigator.userAgent}`;
-
-// window.addEventListener('load', ()=>{
-//   // let long,lat;
-//   // if(navigator.geolocation){
-//   //   navigator.geolocation.getCurrentPosition(position =>{
-//   //     console.log(position);
-//   //   }) 
-//   // }
-//   console.log(navigator)
-  
-// })
-
-// if(navigator.userAgent.match(/iPad/i)|| navigator.userAgent.match(/iPhone/i)){
-//   appDiv.innerHTML = `${navigator.userAgent}`;
-// }
-
-checkVersion = function () {
-    var agent = window.navigator.userAgent,
-    start = agent.indexOf( 'OS ' );
-    if( ( agent.indexOf( 'iPhone' ) > -1 || agent.indexOf( 'iPad' ) > -1 ) && start > -1 ){
-        return window.Number( agent.substr( start + 3, 3 ).replace( '_', '.' ) );
+class createElement {
+    constructor(text, type){
+        this.text = text;
+        this.type = type;
     }
-    return 0;
+    addtoDom(){
+        let app = document.getElementById('app'),
+            node = document.createElement("LI"),
+            text = document.createTextNode(text);
+        node.appendChild(text)
+        app.appendChild(node)
+    }
 }
-const ro = document.getElementById('RO');
-if(!checkVersion){
-    ro.innerHTML='ipad detected'
+
+function addtoDom(textcontent, typeofel){
+    let app = document.getElementById('app'),
+        node = document.createElement("div"),
+        text = document.createTextNode(textcontent);
+        console.log(textcontent,'textcontent',typeofel,'typeofel');
+    node.classList.add(`${typeofel}`);
+    node.appendChild(text)
+    app.appendChild(node)
 }
+
+for (let key in window) {
+    console.log(key,'key', window[key],'value', typeof window[key]);
+    if(typeof window[key] == 'string' || typeof window[key] == 'number'){
+        addtoDom(key, typeof window[key])        
+    }
+  }
