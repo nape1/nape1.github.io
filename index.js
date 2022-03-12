256c0b28e5fd92f43887a20d9913311eb93027bb
nape <najinshah.pearson@gmail.com>
2022-03-12 22:38:55 +0530

    color identifier added

--- index.js
+++ index.js
@@ -25,6 +25,48 @@ function addtoDom(textcontent, typeofel){
 for (let key in window) {
     console.log(key,'key', window[key],'value', typeof window[key]);
     if(typeof window[key] == 'string' || typeof window[key] == 'number'){
-        addtoDom(key, typeof window[key])        
+        // addtoDom(key, typeof window[key])        
     }
+}
+
+_________________________________
+const boxWrap = document.createElement("div");
+async function generateColours(text) {
+  // console.log(text.length);
+  if (text) {
+    text.map((t, i) => {
+      const box = document.createElement("div");
+      const text = document.createElement("h5");
+
+      box.className = "box";
+      box.appendChild(text);
+      boxWrap.appendChild(box);
+
+      text.innerText = t;
+      box.style.backgroundColor = t;
+
+      let wrapper = document.getElementById("app");
+      wrapper.appendChild(boxWrap);
+      return null;
+    });
   }
+}
+
+const getcoloursBtn = document.getElementById("getcolours");
+const resetBtn = document.querySelectorAll('button[type="reset"]')[0];
+var inputBox = document.querySelectorAll("#input textarea")[0];
+
+resetBtn.addEventListener("click", () => {
+  inputBox.value = "";
+});
+
+getcoloursBtn.addEventListener("click", async function () {
+  await generateColours([]);
+  let colourArray = inputBox.value.match(
+    /#(([0-9a-fA-F]{2}){3,4}|([0-9a-fA-F]){3,4})/g
+  );
+
+  // boxWrap.innerHTML = "";
+  await generateColours(colourArray);
+  console.log(colourArray);
+});
